216673f90433ac58c5ca46719e6a03b2bcd0f9ae
Bigodeiro <enzobigber@gmail.com>
2023-10-24 20:57:11 -0300

    Grid Working!

--- main.cpp
+++ main.cpp
@@ -4,16 +4,75 @@
 #include <cmath>
 #include <math.h>
 
-#define WIDTH 1280
-#define HEIGTH 960
+#define WIDTH 900
+#define HEIGTH 900
+#define GRIDSIZE 9
 
+class cell 
+{
+  public:
+    sf::Vector2f pos;
+    int nPossib;
+
+    int possibleCellsUp[16] = {0};
+    int possibleCellsDown[16] = {0};
+    int possibleCellsLeft[16] = {0};
+    int possibleCellsRight[16] = {0};
+
+    //variable to store path to image
+    std::string imgPath;
+    
+
+    cell()
+    {
+        pos.x = 0;
+        pos.y = 0;
+
+        imgPath = "img/0.png";
+    };
+};
+
+void drawGrid(sf::RenderWindow &window, cell grid[GRIDSIZE][GRIDSIZE])
+{
+    for (int i = 0; i < GRIDSIZE; i++)
+    {
+        for (int j = 0; j < GRIDSIZE; j++)
+        {
+            sf::Sprite sprite;
+            sf::Texture texture;
+            texture.loadFromFile(grid[i][j].imgPath);
+            sprite.setTexture(texture);
+            sprite.setPosition(grid[i][j].pos);
+
+            window.draw(sprite);
+        }
+    }
+}
 
 int main()
 {
     //*tela e view
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGTH), "SFML!", sf::Style::Close | sf::Style::Titlebar);
-    sf::View view(sf::Vector2f(0,0), sf::Vector2f(WIDTH, HEIGTH));
+    sf::View view(sf::Vector2f(GRIDSIZE * 9 / 2, GRIDSIZE * 9 / 2), sf::Vector2f(GRIDSIZE * 10, GRIDSIZE * 10));
     window.setView(view);
+
+    //declare the map grid
+    cell grid[GRIDSIZE][GRIDSIZE];
+    for (int i = 0; i < GRIDSIZE; i++)
+    {
+        for (int j = 0; j < GRIDSIZE; j++)
+        {
+            grid[i][j].pos.x = i * 9;//multiplique por 10 para separar as celulas
+            grid[i][j].pos.y = j * 9;
+        }
+    }
+
+    grid[0][0].imgPath = "img/2.png";
+    grid[1][0].imgPath = "img/9.png";
+    grid[2][0].imgPath = "img/9.png";
+    grid[3][0].imgPath = "img/9.png";
+    grid[4][0].imgPath = "img/4.png";
+
 
 
     while (window.isOpen())//! cada iteração aqui é um frame
@@ -31,14 +90,16 @@ int main()
             }
         }
 
-    //!codigo a ser executado todo frame
+    //!code that will be executed every frame
     window.clear();
 
-    //? renderiza algo para a tela, modificando-a
+    //? render something on the screen
 
-    //window.draw()
+    //*draw the grid
+    drawGrid(window, grid);
 
-    //? recarrega a tela, fazendo com que as mudanças sejam efetivadas
+
+    //? refresh the screen applying the changes
     window.display();
 
     }
